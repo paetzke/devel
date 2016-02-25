2f5b3f33ebcd2766aada8b3858c423c089ee5d34
Friedrich Paetzke <f.paetzke@gmail.com>
2016-02-25 19:20:00 +0100

    Update fish_prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,3 +1,45 @@
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '↑'
+set __fish_git_prompt_char_upstream_behind '↓'
+
 function fish_prompt
-        echo (pwd) "> "
+        set last_status $status
+
+        set_color normal
+        printf '['
+        printf '%s ' (date "+%H:%M:%S")
+        set_color purple
+        printf '%s@%s ' (whoami) (hostname|cut -d . -f 1)
+        set_color cyan
+        printf '%s' (pwd | sed -e "s|^$HOME|~|")
+        set_color normal
+        printf ']'
+
+        printf '%s' (__fish_git_prompt)
+        set_color normal
+        if [ $last_status = 0 ]
+                set_color green
+        else
+                set_color red
+        end
+
+        printf '\n'
+        if test $VIRTUAL_ENV
+                printf "(%s) " (basename $VIRTUAL_ENV)
+        end
+
+        if [ $last_status = 0 ]
+                printf 'λ '
+        else
+                printf 'ϵ '
+        end
+        set_color normal
 end
